79e54933bb3d99434237b67573b0ae2d6e624c6f
Alisson R S Prestes <javalisson@gmail.com>
2020-02-02 01:37:08 -0300

    Sprite do personagem pisca ao tomar dano e, com novo material, deixa de enroscar nas plataformas

--- Assets/scripts/TomaDano.cs
+++ Assets/scripts/TomaDano.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class TomaDano : MonoBehaviour
 {
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "perigo")
         {
-            Debug.Log("Ai aiiiiiiiii");
+            StartCoroutine(Pisca());
         }
+    }
+
+    IEnumerator Pisca()
+    {
+        float newX = transform.position.x - 5f;
+        spriteRenderer.color = new Color32(255, 92, 45, 255);
+        if (newX < 0f) newX = 0f;
+        transform.position = new Vector3(newX, 0f, 0f);
+        for (var i = 0; i < 10; i++)
+        {
+            spriteRenderer.enabled = !spriteRenderer.enabled;
+            yield return new WaitForSeconds(0.3f);
+        }
+        spriteRenderer.color = new Color32(255, 255, 255, 255);
     }
 }
